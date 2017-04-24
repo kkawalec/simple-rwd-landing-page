1db1bec14f07af35ff55c4ac20b47216d5924216
Aedriel <aedriell@gmail.com>
2017-04-24 16:16:57 +0200

    desktop view - almost finished

--- js/app.js
+++ js/app.js
@@ -4,16 +4,55 @@
 const iphoneImage = document.querySelector('.js-iphone-image');
 const iphoneImageParent = iphoneImage.parentElement;
 
+const mainImageDiv = document.querySelector('.main-image');
+const navigationBar = document.querySelector('.navigation');
+
+
+// const windowHeight = document.documentElement.clientHeight
+
+// console.dir(iphoneImageParent)
+// console.log(windowWidth, windowHeight)
 /**
  * Functions
  */
-function resizeIPhoneImage() {
-    const spaceHeight = iphoneImageParent.offsetTop + iphoneImageParent.clientHeight
-    console.dir(iphoneImage)
+
+/**
+ * Resizing IphoneImage
+ */
+function resizeIPhoneImageDesktop() {
+    const spaceHeight = iphoneImageParent.offsetTop + iphoneImageParent.clientHeight;
     const iPhoneHeight = Math.min(spaceHeight*0.72, 874);
     iphoneImage.style.height = `${iPhoneHeight}px`;
-    iphoneImage.style.width = 'auto'
+    iphoneImage.style.width = 'auto';
 }
+
+function resizeIPhoneImage() {
+    const windowWidth = document.documentElement.clientWidth
+
+    if(windowWidth > 768) {
+        resizeIPhoneImageDesktop();
+    }
+
+}
+
+/**
+ * Resizing NavigationBar height
+ */
+function resizeNavigationBarDesktop() {
+    const mainImageHeight = mainImageDiv.clientHeight;
+    navigationBar.style.height = `${Math.ceil(mainImageHeight * 14.75 / 100)}px`;
+}
+
+function resizeNavigationBar() {
+    const mainImageHeight = mainImageDiv.clientHeight;
+    const windowWidth = document.documentElement.clientWidth;
+
+    if(windowWidth > 768) {
+        resizeNavigationBarDesktop();
+    }
+}
+
+
 
 /**
  * Event listeners
@@ -24,3 +63,9 @@ function resizeIPhoneImage() {
  */
 window.addEventListener('resize', resizeIPhoneImage);
 document.addEventListener('DOMContentLoaded',  resizeIPhoneImage);
+
+/**
+ * Resize navigation bar
+ */
+window.addEventListener('resize', resizeNavigationBar);
+document.addEventListener('DOMContentLoaded',  resizeNavigationBar);
